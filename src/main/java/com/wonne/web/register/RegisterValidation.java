fd3206d8b9ffa76861d3d0df8b92724c600bee17
Unknown <hrayapudi@wonne.com>
2018-06-21 23:10:57 -0400

    Added server side validation for registration

--- src/main/java/com/wonne/web/register/RegisterValidation.java
+++ src/main/java/com/wonne/web/register/RegisterValidation.java
@@ -1,18 +1,31 @@
 package com.wonne.web.register;
 
-import static com.wonne.web.util.WonneUtil.*;
-
 import org.slf4j.*;
-
+import java.util.regex.*;
+import javax.mail.internet.*;
 import com.wonne.web.database.*;
 
+import static com.wonne.web.util.WonneUtil.*;
 import static com.wonne.web.register.ValidationResult.*;
 
 
 public final class RegisterValidation{
 
-    private final static String NAME    = RegisterValidation.class.getSimpleName( );
-    private final static Logger LOGGER  = LoggerFactory.getLogger( NAME );
+    private final static int FULLNAME_LENGTH    = 3;
+    private final static String FULLNAME_ERR_MSG= "Full name can't be less than " + FULLNAME_LENGTH + " letters!";
+     
+    private final static int PASSWORD_LENGTH    = 8;
+    private final static String PASSWORD_ERR_MSG= "Password must be at least " + PASSWORD_LENGTH + " letters, must contain at least one number & one uppercase letter.";
+    
+    private final static int ZIP_LENGTH         = 5;
+    private final static String ZIP_REGEX       = "[0-9]+";
+    
+    private final static int PHONE_LENGTH       = 10;
+    private final static String PHONE_REGEX     = "[0-9]+";
+    
+    private final static Pattern _1_LETTER      = Pattern.compile( "[0-9]" );
+    private final static String NAME            = RegisterValidation.class.getSimpleName( );
+    private final static Logger LOGGER          = LoggerFactory.getLogger( NAME );
     
     
     public final static ValidationResult validate( RegisterBean bean ){
@@ -22,79 +35,119 @@ public final class RegisterValidation{
         }
         
         
-        if( bean.getFullName() == null || bean.getFullName().trim( ).length( ) < 3 ){
-            return createInvalid( "Full name can't be less than 3 letters!" );            
+        if( bean.getFullName() == null || bean.getFullName().trim( ).length( ) < FULLNAME_LENGTH ){
+            return createInvalid( FULLNAME_ERR_MSG );            
         }
         
-        //Check Email address
         
-        
-        if( !isValid(bean.getPassword()) ){
-            request.setAttribute("error", "Password is invalid!");
-            request.getRequestDispatcher("/Register.html").forward(request, response);
-            return false;
+        if( !isValidEmail( bean.getEmail()) ){
+            return createInvalid( "Email address is Invalid!" );
         }
         
-        if( !bean.getPassword().equals(bean.getRepassword()) ){
-            request.setAttribute("error", "Passwords do not match!");
-            request.getRequestDispatcher("/Register.html").forward(request, response);
-            return false;
+        //Must be at least 8 characters and must contain at least one number & one uppercase letter
+        if( !isValidPassword( bean.getPassword()) ){
+            return createInvalid( PASSWORD_ERR_MSG );
         }
+        
+        
+        if( !bean.getPassword().equals( bean.getRepassword( )) ){
+            return createInvalid( "Passwords do not match!" );
+        }
+        
         
         if( !isValid(bean.getCompanyName()) ){
-            request.setAttribute("error", "Company name is invalid!");
-            request.getRequestDispatcher("/Register.html").forward(request, response);
-            return false;
+            return createInvalid( "Company name is invalid!");            
         }
                 
-        if( !isValid(bean.getPhone()) ){
-            request.setAttribute("error", "Phone number is invalid!");
-            request.getRequestDispatcher("/Register.html").forward(request, response);
-            return false;
+        if( !isValidPhoneNumnber(bean.getPhone()) ){
+            return createInvalid("Phone number is invalid!");            
         }
                 
         if( !isValid(bean.getAddress()) ){
-            request.setAttribute("error", "Address is invalid!");
-            request.getRequestDispatcher("/Register.html").forward(request, response);
-            return false;
+            return createInvalid("Address is invalid!");            
         }
 
         if( !isValid(bean.getCity()) ){
-            request.setAttribute("error", "City is invalid!");
-            request.getRequestDispatcher("/Register.html").forward(request, response);
-            return false;
-        }
-        
+            return createInvalid("City is invalid!");            
+        }        
         
         if( !isValid(bean.getState()) ){
-            request.setAttribute("error", "State is invalid!");
-            request.getRequestDispatcher("/Register.html").forward(request, response);
-            return false;
+            return createInvalid("State is invalid!");            
         }
         
-        
-        if( !isValid(bean.getZip()) ){
-            request.setAttribute("error", "Zipcode is invalid!");
-            request.getRequestDispatcher("/Register.html").forward(request, response);
-            return false;
+        if( !isValidZip(bean.getZip()) ){
+            return createInvalid("Zipcode is invalid!");            
         }
-        
-        
+                
         if( !isValid(bean.getOrganization()) ){
-            request.setAttribute("error", "Organization selection is invalid!");
-            request.getRequestDispatcher("/Register.html").forward(request, response);
-            return false;
+            return createInvalid("Organization selection is invalid!");            
         }
         
         if( !isValid(bean.getRole()) ){
-            request.setAttribute("error", "Role selection is invalid!");
-            request.getRequestDispatcher("/Register.html").forward(request, response);
-            return false;
+            return createInvalid("Role selection is invalid!");            
         }
         
-        
-        return null;
+        return createValid( );
     
+    }
+    
+    
+    protected final static boolean isValidZip( String zip ) {
+        if ( !isValid(zip) ) return false;        
+        return (zip.matches(ZIP_REGEX) && zip.length() == ZIP_LENGTH );        
+    }
+
+    
+    protected final static boolean isValidPhoneNumnber( String phone ) {
+        if ( !isValid(phone) ) return false;
+        
+        phone = phone.replaceAll( "-", "" );
+        phone = phone.replaceAll( "(", "" );
+        phone = phone.replaceAll( ")", "" );
+        
+        return (phone.matches(PHONE_REGEX) && phone.length() == PHONE_LENGTH );
+        
+    }
+
+    protected final static boolean isValidPassword( String password ) {
+        
+        if ( !isValid(password) ) return false;
+        if( password.length( ) != PASSWORD_LENGTH ) return false;
+
+        //Doesn't have at least 1 Upper case letter
+        if( password.equals(password.toLowerCase()) ) return false;
+        
+        //Doesn't have at least 1 number
+        if( !_1_LETTER.matcher( password ).find() ) return false;
+        
+        return true;
+    }
+
+
+    protected final static boolean isValidEmail( String email ){
+        
+        if ( !isValid(email) ) return false;
+                
+        boolean result = true;
+        try{
+            
+            new InternetAddress( email );
+            if( !hasNameAndDomain(email) ){
+                result = false;
+            }
+        
+        }catch( AddressException ex ){
+          result = false;
+        }
+        
+        return result;
+      
+    }
+    
+
+    private final static boolean hasNameAndDomain( String email ){
+        String[] tokens = email.split("@");
+        return tokens.length == 2 && isValid(tokens[0]) && isValid(tokens[1]);      
     }
     
 }
